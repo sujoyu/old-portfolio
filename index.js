6d136c650abdb4a427e151a937b3ef62b8d0b7fb
sujoyu <sujoyu.motoki@gmail.com>
2016-12-11 23:40:08 +0900

    プロフィールを追加

---
+++ index.js
@@ -0,0 +1,22 @@
+$(function() {
+
+	$('#navigation').find('a').click(function() {
+		var id = $(this).attr('href');
+		console.log($(id).offset().top)
+		$('body').animate({scrollTop:$(id).find('h2').offset().top});
+	})
+
+	// メールスパマー対策
+	setTimeout(function() {
+		var encoded = 'vyou}7wz|yxPxt}Dz';
+		var offset = 3;
+		var decoded = Array.prototype.map.call(encoded, function(c, i) {
+			return String.fromCharCode(c.charCodeAt(0) - offset - i);
+		}).join('');
+
+		$('#contact').html($('<a></a>')
+			.addClass('grey-text text-lighten-3')
+			.attr('href', 'mailto:' + decoded)
+			.text(decoded));
+	}, 5000)
+})
